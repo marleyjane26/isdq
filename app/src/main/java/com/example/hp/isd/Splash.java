200c63a872c35d67d392f5f33013c1300896d7c2
marleyjane26 <40754729+marleyjane26@users.noreply.github.com>
2019-04-12 14:30:00 +0300

    intial

--- app/src/main/java/com/example/hp/isd/Splash.java
+++ app/src/main/java/com/example/hp/isd/Splash.java
@@ -1,13 +1,40 @@
 package com.example.hp.isd;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Splash extends AppCompatActivity {
+    private static int timeout=5000;
+    TextView txt;
+    ImageView img;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        txt=findViewById(R.id.txt);
+        img=findViewById(R.id.img);
+
+        Animation anim = AnimationUtils.loadAnimation(Splash.this,R.anim.myanim);
+        img.startAnimation(anim);
+        txt.startAnimation(anim);
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(Splash.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },timeout);
     }
 }
--- app/src/main/java/com/example/hp/isd/payment.java
+++ app/src/main/java/com/example/hp/isd/payment.java
@@ -1,4 +1,28 @@
 package com.example.hp.isd;
 
-public class payment {
+import android.annotation.SuppressLint;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class payment  {
+    private int client_id;
+    private double balance;
+    private int payment_state;
+    String issued_date;
+    String payed_date;
+
+    public   payment(int client_id,double balance, int payment_state){
+
+        this.client_id=client_id;
+        this.balance=balance;
+        this.payment_state=payment_state;
+
+        }
+
+
+
+
+
+
 }
