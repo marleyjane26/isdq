6a9f0fff3c18f61913f9f367e7e708d40277a0e4
marleyjane26 <40754729+marleyjane26@users.noreply.github.com>
2019-04-12 17:03:37 +0300

    second

--- app/src/main/java/com/example/hp/isd/MainActivity.java
+++ app/src/main/java/com/example/hp/isd/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.hp.isd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    Button btnSignIN;
+    EditText edtEmail,edtPass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        btnSignIN=(Button) findViewById(R.id.btn_login);
+        edtEmail=(EditText) findViewById(R.id.input_email);
+        edtPass=(EditText) findViewById(R.id.input_password);
+       FirebaseAuth.AuthStateListener mAuthListner;
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
+
 }
